7b04dd02978cd5054facb88f5204687a68fd9259
Santhoshraj KG <santhosh.gvelu@gmail.com>
2022-09-10 23:06:46 +0530

    Final commit

--- app.py
+++ app.py
@@ -4,10 +4,10 @@ class gridgame():
     
     def __init__(self): 
         print("Player 1 enter the initial values..eg(p1 p2 p3 p4 p5) :")
-        p1="P3 P4 P5 P1 P2" #input()
+        p1=input() # "P3 P4 P5 P1 P2"#
         p1=list(p1.split(" "))
         print("Player 2 enter the initial values..eg(p1 p2 p3 p4 p5) :")
-        p2="P1 P2 P3 P4 P5" #input()
+        p2=input() # "P1 P2 P3 P4 P5"#
         p2=list(p2.split(" "))
         for i in range(5):
             self.board[4][i]="A-"+p1[i]
@@ -30,9 +30,20 @@ class gridgame():
                         newj=j-1
                     elif((cmd[3:]=='R' and player=='A') or (cmd[3:]=='L' and player=='B')):
                         newj=j+1
+                    if(self.board[newi][newj]!='....' and  self.board[newi][newj][:1]==player):
+                        print("Targeted a friendly character...")
+                        return False
                     self.board[newi][newj]=self.board[i][j]
                     self.board[i][j]='....'
                     return True
+    def checkboard(self,player):
+        for i in range(5):
+            for j in range(5):
+                if(self.board[i][j]=='....'):
+                    continue
+                elif(self.board[i][j][:1]==player):
+                    return False
+        return True
 
     def printboard(self):
         for i in self.board:
@@ -48,13 +59,19 @@ while(opt=='Y'):
         if grid.changeBoard('A',p1):
             break
     grid.printboard()
+    if(grid.checkboard('B')):
+        print("Player A won the game !!!")
+        break
     while(1):
         print("Enter the Player2 command...")
         p2=input()
         if grid.changeBoard('B',p2):
             break
     grid.printboard()
-    opt=input("Y/N to continue?.....")
+    if(grid.checkboard('A')):
+        print("Player B won the game !!!")
+        break
+    opt='Y'#input("Y/N to continue?.....")
 
 
 
